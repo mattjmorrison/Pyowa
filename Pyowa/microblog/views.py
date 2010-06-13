ee865792b84435c51d837c4f21e6412752f2650a
Matthew J Morrison <mattj.morrison@gmail.com>
2010-06-13 18:19:14 -0500

    added template

--- Pyowa/microblog/views.py
+++ Pyowa/microblog/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+from django import shortcuts
 
 
 def index(request):
-    return HttpResponse("There!!!")
+    return shortcuts.render_to_response("index.html")
+    #return HttpResponse("There!!!")
